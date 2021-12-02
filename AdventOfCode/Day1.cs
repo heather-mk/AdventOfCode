e8a2665d7b8921669ef80f0343e031fd97376c4b
Heather K <16761638+heather-mk@users.noreply.github.com>
2021-12-02 11:25:42 -0600

    Day 1 completed

---
+++ AdventOfCode/Day1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AdventOfCode
+{
+    class Day1
+    {
+        private string _inputFilePath;
+        private const string _question = "How many measurements are larger than the previous measurment?";
+        public string InputFilePath
+        {
+            get
+            {
+                return _inputFilePath;
+            }
+            set
+            {
+                if (!System.IO.File.Exists(value))
+                {
+                    throw new ArgumentException("Input file does not exist.");
+                }
+                _inputFilePath = value;
+            }
+        }
+        public string Question
+        {
+            get { return _question; }
+        }
+
+        public Day1(string inputPath)
+        {
+            InputFilePath = inputPath;
+        }
+
+        public int GetAnswer()
+        {
+            int prevReading = 0, curReading = 0, numIncreases = 0;
+
+            using (StreamReader sr = new StreamReader(InputFilePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    if (!int.TryParse(sr.ReadLine(), out curReading))
+                    {
+                        throw new ArgumentException("File path does not contain a parseable file.");
+                    }
+                    if (prevReading != 0 && curReading > prevReading)
+                    {
+                        numIncreases++;
+                    }
+                    prevReading = curReading;
+                }
+                sr.Close();
+            }
+            return numIncreases;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -10,6 +10,13 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
+            // something for each day
+            string d1_path = System.IO.Path.GetDirectoryName(System.IO.Path.Combine(System.Reflection.Assembly.GetExecutingAssembly().Location));
+            var d1 = new Day1(System.IO.Path.Combine(d1_path, "input","day1_input.txt"));
+            Console.Write(d1.Question + " ");
+            Console.Write(d1.GetAnswer());
+
+            Console.ReadKey();
         }
     }
 }
